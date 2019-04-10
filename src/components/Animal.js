d8cf63034648c3f36c84dda06667056a97f0a3c5
Christian Anderson <ca96187@gmail.com>
2019-04-10 10:48:38 -0600

    created Animal and Animals components

---
+++ src/components/Animal.js
@@ -0,0 +1,20 @@
+// this is what we will display for each animal
+
+import React, { Component } from "react"
+
+export default class Animal extends Component {
+
+  render() {
+
+    let { animal } = this.props
+
+    return (
+      <div style={{ border: "1px solid #eee", margin: 10 }}>
+        <img src={animal.imgUrl} alt="" width="200" />
+        <p>species: {animal.species}</p>
+        <p>qty: {animal.quantity}</p>
+      </div>
+    )
+  }
+}
+
---
+++ src/components/Animals.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+import axios from "axios"
+
+import Animal from "./Animal"
+
+export default class Animals extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      animals: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("/api/animals") // we only put the endpoint because we indicated a proxy in package.json
+      .then(res => {
+        this.setState({
+          animals: res.data
+        })
+      })
+      .catch(err => console.log("error get animals: ", err))
+  }
+
+  render() {
+    return (
+      this.state.animals.map(animal => {
+        return <Animal key={animal.id} animal={animal} />
+      })
+    )
+  }
+}
